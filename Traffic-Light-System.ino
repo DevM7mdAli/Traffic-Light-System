62dde43ca428c5faa959204da54a1cc9acbc9278
DevM7mdAli <DM7A+Github@outlook.sa>
2024-07-25 01:09:52 +0300

    finish

--- Traffic-Light-System.ino
+++ Traffic-Light-System.ino
@@ -1,15 +1,5 @@
 
-void setup() {
-  pinMode(2, OUTPUT); // red light stop
-  pinMode(3, OUTPUT); // yellow light slow down
-  pinMode(4, OUTPUT); // green light go
-    Serial.begin(9600);
-    Serial.print ("start the code\n");
-}
-
-// the loop function runs over and over again forever
-void loop() {
-  Serial.begin(9600);  // for printing
+void redLight(){
   digitalWrite(2, HIGH);
   Serial.print ("Red light stop\n");
   for (int i = 5; i > 0 ; i--){
@@ -20,8 +10,9 @@ void loop() {
       delay(1000);
   }
   digitalWrite(2, LOW); // red light off
+}
 
-
+void greenLight(){
   digitalWrite(4, HIGH);
   Serial.print ("Green light go\n");
   for (int i = 15; i > 0 ; i--){
@@ -43,10 +34,11 @@ void loop() {
       Serial.print ("\n");
       delay(1050);
   }
-digitalWrite(4, LOW); // green light off
+  digitalWrite(4, LOW); // green light off
+}
 
-
-  digitalWrite(3, HIGH);
+void yellowLight(){
+    digitalWrite(3, HIGH);
   Serial.print ("Green light go\n");
   for (int i = 4; i > 0 ; i--){
           Serial.print ("Will be red after ");
@@ -59,5 +51,21 @@ digitalWrite(4, LOW); // green light off
           delay(600);
           continue;
       }
-digitalWrite(3, LOW);
+  digitalWrite(3, LOW);
+}
+
+void setup() {
+  pinMode(2, OUTPUT); // red light stop
+  pinMode(3, OUTPUT); // yellow light slow down
+  pinMode(4, OUTPUT); // green light go
+    Serial.begin(9600);
+    Serial.print ("start the code\n");
+}
+
+
+void loop() {
+  Serial.begin(9600);  // for printing
+    redLight();
+    greenLight();
+    yellowLight();
 }
