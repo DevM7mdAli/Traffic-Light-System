ec1377b419865cebeab621487b33f4709a066edb
DevM7mdAli <DM7A+Github@outlook.sa>
2024-07-30 16:26:16 +0300

    adding button for fast mode and enhance code

--- Traffic-Light-System.ino
+++ Traffic-Light-System.ino
@@ -1,63 +1,88 @@
+// pins numbers
+const int red = 2;
+const int yellow = 3; 
+const int green = 4; 
+const int btn = 5 ; 
 
-void redLight(){
-  digitalWrite(2, HIGH);
+int fastMode(){
+  if(digitalRead(btn) == 1){
+    Serial.print("fast Mode is On\n");
+    return 300;
+  } else {
+    return 1000;
+  }
+}
+
+int flashingLightMode(){
+    if(digitalRead(btn) == 1){
+    Serial.print("Flash fast Mode is On\n");
+    return 300;
+  } else {
+    return 600;
+  }
+}
+
+void redLight(int time){
+  digitalWrite(red, HIGH);
   Serial.print ("Red light stop\n");
-  for (int i = 5; i > 0 ; i--){
+  for (int i = time; i > 0 ; i--){
       Serial.print ("Will be green after ");
       Serial.print (i);
       Serial.print (" seconds");
       Serial.print ("\n");
-      delay(1000);
+      delay(fastMode());
   }
-  digitalWrite(2, LOW); // red light off
+  digitalWrite(red, LOW); // red light off
 }
 
-void greenLight(){
-  digitalWrite(4, HIGH);
-  Serial.print ("Green light go\n");
-  for (int i = 15; i > 0 ; i--){
+
+void greenLight(int time){
+  digitalWrite(green, HIGH);
+  Serial.print("Green light go\n");
+  for (int i = time; i > 0 ; i--){
       // for the soon cloosing
       if(i <= 5){
           Serial.print ("Will be yellow after ");
           Serial.print (i);
           Serial.print (" seconds Hurry!");
           Serial.print ("\n");
-          digitalWrite(4, LOW);
-          delay(600);
-          digitalWrite(4, HIGH);
-          delay(600);
+          digitalWrite(green, LOW);
+          delay(flashingLightMode());
+          digitalWrite(green, HIGH);
+          delay(flashingLightMode());
           continue;
       }
       Serial.print ("Will be yellow after ");
       Serial.print (i);
       Serial.print (" seconds");
       Serial.print ("\n");
-      delay(1050);
+      delay(fastMode());
   }
-  digitalWrite(4, LOW); // green light off
+  digitalWrite(green, LOW); // green light off
 }
 
-void yellowLight(){
-    digitalWrite(3, HIGH);
+void yellowLight(int time){
+  digitalWrite(yellow, HIGH);
   Serial.print ("Green light go\n");
-  for (int i = 4; i > 0 ; i--){
+  for (int i = time; i > 0 ; i--){
           Serial.print ("Will be red after ");
           Serial.print (i);
           Serial.print (" seconds Hurry!");
           Serial.print ("\n");
-          digitalWrite(3, LOW);
-          delay(600);
-          digitalWrite(3, HIGH);
-          delay(600);
+          digitalWrite(yellow, LOW);
+          delay(flashingLightMode());
+          digitalWrite(yellow, HIGH);
+          delay(flashingLightMode());
           continue;
       }
-  digitalWrite(3, LOW);
+  digitalWrite(yellow, LOW);
 }
 
 void setup() {
-  pinMode(2, OUTPUT); // red light stop
-  pinMode(3, OUTPUT); // yellow light slow down
-  pinMode(4, OUTPUT); // green light go
+  pinMode(red, OUTPUT); 
+  pinMode(yellow, OUTPUT); 
+  pinMode(green, OUTPUT); 
+  pinMode(btn , INPUT);
     Serial.begin(9600);
     Serial.print ("start the code\n");
 }
@@ -65,7 +90,8 @@ void setup() {
 
 void loop() {
   Serial.begin(9600);  // for printing
-    redLight();
-    greenLight();
-    yellowLight();
+  //prameters for time ON
+    redLight(5);
+    greenLight(10);
+    yellowLight(3);
 }
